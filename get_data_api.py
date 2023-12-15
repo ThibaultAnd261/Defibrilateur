16ed6b78aabfac71c75e725faa740579535b1228
ThibaultAnd261 <thibaultandwork@gmail.com>
2023-12-15 14:40:56 +0100

    Api call for population working

--- get_data_api.py
+++ get_data_api.py
@@ -1,4 +1,8 @@
 import csv
+import pandas as pd
+import requests
+
+FILENAME = "geodae.csv"
 
 def read_file (filename):
 
@@ -11,12 +15,25 @@ def read_file (filename):
         
     return l
 
+def api_call(codeInsee):
+    response = requests.get('https://geo.api.gouv.fr/communes/'+codeInsee+'?fields=nom,population&format=json&geometry=centre')
+    # print('https://geo.api.gouv.fr/communes/'+codeInsee+'?fields=nom,population&format=json&geometry=centre') 
+    print(response.json())
     
 
-# def main():
-#     data = read_file(FILENAME)
-#     # print (data)
-#     df = pd.DataFrame(data)
-#     print(df)
+def main():
+    data = read_file(FILENAME)
+    # print (data)
+    df = pd.DataFrame(data)
+    # print(df)
+    # print(df.keys())
+    # print(df['c_com_insee'])
 
-# main()
+    # print(df['c_com_insee'][0])
+
+    api_call(df['c_com_insee'][0])
+
+    # for index, row in df.iterrows():
+    #     print(row['c_com_insee'])
+
+main()
