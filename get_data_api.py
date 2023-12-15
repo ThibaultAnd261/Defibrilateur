9a8a8a4527f8845265f01bcb7df066425bc72c72
ThibaultAnd261 <thibaultandwork@gmail.com>
2023-12-15 14:54:45 +0100

    Api call for 10 rows working

--- get_data_api.py
+++ get_data_api.py
@@ -4,6 +4,9 @@ import requests
 
 FILENAME = "geodae.csv"
 
+# liste_pop = [{}]
+liste_pop = dict()
+
 def read_file (filename):
 
     with open (filename, mode = 'r', encoding= 'utf8') as f :
@@ -17,23 +20,21 @@ def read_file (filename):
 
 def api_call(codeInsee):
     response = requests.get('https://geo.api.gouv.fr/communes/'+codeInsee+'?fields=nom,population&format=json&geometry=centre')
-    # print('https://geo.api.gouv.fr/communes/'+codeInsee+'?fields=nom,population&format=json&geometry=centre') 
-    print(response.json())
+    resJson = response.json()
+    nom = resJson['nom']
+    population = resJson['population']
+    liste_pop[nom] = population
     
 
 def main():
     data = read_file(FILENAME)
-    # print (data)
     df = pd.DataFrame(data)
-    # print(df)
-    # print(df.keys())
-    # print(df['c_com_insee'])
 
-    # print(df['c_com_insee'][0])
+    for index, row in df.iterrows():
+        if index == 10:
+            break
+        api_call(row['c_com_insee'])
 
-    api_call(df['c_com_insee'][0])
-
-    # for index, row in df.iterrows():
-    #     print(row['c_com_insee'])
+    print(liste_pop)
 
 main()
