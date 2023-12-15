457ee01cc8d7ea8e8f21eec583476788c26d6764
ThibaultAnd261 <thibaultandwork@gmail.com>
2023-12-15 17:15:09 +0100

    Merge two dataframes & use csv file instead of API

--- get_data_api.py
+++ get_data_api.py
@@ -1,40 +1,29 @@
 import csv
 import pandas as pd
-import requests
 
-FILENAME = "geodae.csv"
-
-# liste_pop = [{}]
-liste_pop = dict()
+FILENAME_DEFIBRILLATEUR = "geodae.csv"
+FILENAME_COMMUNE = "communes.csv"
 
 def read_file (filename):
-
     with open (filename, mode = 'r', encoding= 'utf8') as f :
         reader = csv.DictReader(f , delimiter = ";")
         l = []
         for ligne in reader :
             l.append(ligne)
     
-        
     return l
 
-def api_call(codeInsee):
-    response = requests.get('https://geo.api.gouv.fr/communes/'+codeInsee+'?fields=nom,population&format=json&geometry=centre')
-    resJson = response.json()
-    nom = resJson['nom']
-    population = resJson['population']
-    liste_pop[nom] = population
-    
-
 def main():
-    data = read_file(FILENAME)
-    df = pd.DataFrame(data)
+    dataDefibrilateur = read_file(FILENAME_DEFIBRILLATEUR)
+    dfDef = pd.DataFrame(dataDefibrilateur)
 
-    for index, row in df.iterrows():
-        if index == 10:
-            break
-        api_call(row['c_com_insee'])
+    dataCommune = read_file(FILENAME_COMMUNE)
+    dfCom = pd.DataFrame(dataCommune)
 
-    print(liste_pop)
+    dfCom = dfCom.rename(columns={'DEPCOM': 'c_com_insee'})
+    merged_df = pd.merge(dfDef, dfCom, on='c_com_insee', how='inner')
+
+    # print(merged_df)
+    # print(merged_df.keys())
 
 main()
