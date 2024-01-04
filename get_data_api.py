f1cd219e38c6825cfc1d148d25ae8ed6955f69ed
ThibaultAnd261 <thibaultandwork@gmail.com>
2024-01-04 23:31:03 +0100

    Histogram done

--- get_data_api.py
+++ get_data_api.py
@@ -6,7 +6,7 @@ FILENAME_COMMUNE_2019 = "communes.csv"
 FILENAME_COMMUNE_2021 = "donnees_communes.csv"
 
 # retourne le contenu du fichier sous forme de liste
-def read_file (filename):
+def read_file (filename: str) -> []:
     with open (filename, mode = 'r', encoding= 'utf8') as f :
         reader = csv.DictReader(f , delimiter = ";")
         l = []
@@ -15,8 +15,8 @@ def read_file (filename):
     
     return l
 
-# fusionne les dataframes en une seule manipulable
-def get_merged_dataframe():
+# fusionne les dataframes en une seule manipulable et la retourne
+def get_merged_dataframe() -> pd.DataFrame:
     dataDefibrilateur = read_file(FILENAME_DEFIBRILLATEUR)
     dfDef = pd.DataFrame(dataDefibrilateur) # transformation de la data sur les défibrillateurs en dataframe 
 
@@ -32,30 +32,34 @@ def get_merged_dataframe():
     merged_df = pd.merge(merged_df, dfCom2[['c_com_insee', 'REG']], on='c_com_insee', how='left') # merge de merged_df et df3 sur merged_df
 
     merged_df['PTOT'] = pd.to_numeric(merged_df['PTOT']) # conversion en nombre
-    merged_df = merged_df.drop(['REG_y'], axis=1)
-    merged_df = merged_df.rename(columns={'REG_x' : 'REGION'})
+    merged_df = merged_df.rename(columns={'REG' : 'REGION'})
 
     return merged_df
 
+# retourne les horaires possibles d'après la dataframe
+def get_horaires_dispo(horaire_values: pd.Series) -> set[str]:
+    horaires_dispo = set()
+
+    for value in horaire_values:
+        obj_list = value.split(',')
+
+        # itére sur les objets et ajoute les horaires au set()
+        for obj in obj_list:
+            obj = obj.strip('{}')
+            horaires_dispo.add(obj)
+
+    return horaires_dispo
 
 # def main():
-#     dataDefibrilateur = read_file(FILENAME_DEFIBRILLATEUR)
-#     dfDef = pd.DataFrame(dataDefibrilateur)
-
-#     dataCommune = read_file(FILENAME_COMMUNE_2019)
-#     dfCom = pd.DataFrame(dataCommune)
-    
-#     dataCommune2021 = read_file(FILENAME_COMMUNE_2021)
-#     dfCom2 = pd.DataFrame(dataCommune2021)
-#     dfCom2['c_com_insee'] = dfCom2['CODDEP'] + dfCom2['CODCOM'].astype(str)
-
-#     dfCom = dfCom.rename(columns={'DEPCOM': 'c_com_insee'})
-#     merged_df = pd.merge(dfDef, dfCom, on='c_com_insee', how='inner')
-
-#     merged_df = pd.merge(merged_df, dfCom2[['c_com_insee', 'REG']], on='c_com_insee', how='left')
-
-#     # merged_df['categoryVille'] = merged_df.apply(population_category, axis=1)
-#     print(merged_df)
-#     # print(dfCom2)
+#     merged_df = get_merged_dataframe()
+#     # get_disp_j(merged_df)
+#     print(merged_df['c_disp_h'].unique())
+#     # print(len(merged_df['c_disp_h'].unique()))
+#     # print("------")
+#     # print(set(merged_df['c_disp_h'].unique()))
+#     # print(len(set(merged_df['c_disp_h'].unique())))
+#     print("------")
+#     horaire = get_horaires_dispo(merged_df['c_disp_h'].unique())
+#     print(horaire)
 
 # main()
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
-from get_data_api import get_merged_dataframe
+from get_data_api import get_merged_dataframe, get_horaires_dispo
 import plotly.express as px
 import numpy as np
 import folium
@@ -7,46 +7,92 @@ import folium
 # Dataframe mergée 
 df = get_merged_dataframe()
 
+# Horaires disponibles
+horaires_dispo = get_horaires_dispo(df['c_disp_h'].unique())
+
 # création de l'histogramme par défaut
 fig = px.histogram(
     data_frame=df,
-    x=df['PTOT'],
+    x='PTOT',
     labels={'PTOT': 'Nombre d\'habitants', 'count': 'Nombre de défibrillateurs'},
     color='REGION',
     nbins=60
 )
 
+# liste des différents moments de disponibilité d'un défibrillateur
+jours_dispos = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche', '\"jours fériés\"', 'événements', '7j/7']
 
 app = Dash(__name__)
 
+# layout de l'application
 app.layout = html.Div([
     html.H1(children='Défibrillateur en France métropolitaine', style={'textAlign':'center'}),
-    dcc.RadioItems(['Aucun filtre', 'Défibrillateurs validés', 'Défibrillateurs en attente'], 'Aucun filtre', inline=True, id='radio-value'),
-    # dcc.Graph(figure=fig),
+    html.H2('Filtres de l\'histogramme:', style={'margin-top': '20px', 'font-weight': 'bold'}),
+
+    html.Div([
+        html.H3('Type de défibrillateur :'),
+        dcc.RadioItems(['Aucun filtre', 'Défibrillateurs validés', 'Défibrillateurs en attente'], 'Aucun filtre', inline=True, id='radio-value')
+    ], style={'display': 'flex', 'align-items': 'center', 'margin-top': '20px'}),
+    
+    html.Div([
+        html.H3('Disponibilité des défibrillateurs :', style={'margin-top': '10px'}),
+        html.Div([
+            html.H4('Plage journalière :'),
+            dcc.Checklist(
+                options=[{'label': jour_dispo.strip('\"').capitalize(), 'value': jour_dispo} for jour_dispo in jours_dispos],
+                value=[],
+                id='checkbox-jours',
+                inline=True
+            ),
+        ], style={'display': 'flex', 'align-items': 'center', 'margin-top': '10px'}),
+        html.Div([
+            html.H4('Plage horaire :'),
+            dcc.Checklist(
+                options=[{'label': horaire_dispo.strip('\"').capitalize(), 'value': horaire_dispo} for horaire_dispo in horaires_dispo],
+                value=[],
+                id='checkbox-horaires',
+                inline=True
+            )
+        ], style={'display': 'flex', 'align-items': 'center', 'margin-top': '10px'}),
+    ], style={'margin-top': '20px'}),
     dcc.Graph(figure=fig, id='graph-id'),
     dash_table.DataTable(data=df.to_dict('records'))
-])
+], style={'padding' : '25px'})
 
-# @callback(
-#     Output(component_id='graph-id', component_property='figure'),
-#     Input(component_id='radio-value', component_property='value')
-# )
-# def update_graph(filter_selected):
-#     if filter_selected == "Aucun filtre":
-#         filtered_df = df
-#     elif filter_selected == "Défibrillateurs validés":
-#         filtered_df = df[df['c_etat_valid'] == 'validées']
-#     else:
-#         filtered_df = df[df['c_etat_valid'] == 'en attente de validation']
+# fonction callback pour les affichages dynamiques
+@callback(
+    Output(component_id='graph-id', component_property='figure'),
+    Input(component_id='radio-value', component_property='value'),
+    Input(component_id='checkbox-jours', component_property='value'),
+    Input(component_id='checkbox-horaires', component_property='value')
+)
+def update_graph(filter_selected, days_selected, hours_selected):
+    # vérification sur le type de défibrillateur choisi
+    if filter_selected == "Aucun filtre":
+        filtered_df = df
+    elif filter_selected == "Défibrillateurs validés":
+        filtered_df = df[df['c_etat_valid'] == 'validées']
+    else:
+        filtered_df = df[df['c_etat_valid'] == 'en attente de validation']
     
-#     addition_filtered = filtered_df.groupby('categoryVille').size()
-#     villeName_filtered = filtered_df.groupby('categoryVille')['c_com_nom'].nunique()
-#     moyenne_filtered = addition_filtered / villeName_filtered
-    
-#     # new_fig = px.bar(x=moyenne_filtered.index, y=moyenne_filtered.values, labels={'x': 'Nombre d\'habitants','y': 'Nombre moyen de défibrillateur'})
-#     new_fig = px.histogram(x=moyenne_filtered.index, y=moyenne_filtered.values, labels={'x': 'Nombre d\'habitants','y': 'Nombre moyen de défibrillateur'})
-#     return new_fig
+    # vérification sur la/les disponibilité(s) du défibrillateur choisi
+    if days_selected:
+        filtered_df = filtered_df[filtered_df['c_disp_j'].apply(lambda x: any(jour_evenement in x for jour_evenement in days_selected))]
 
+    # vérification sur les/l'heure(s) de disponibilité du défibrillateur
+    if hours_selected:
+        filtered_df = filtered_df[filtered_df['c_disp_h'].apply(lambda x: any(horaire in x for horaire in hours_selected))]
+
+    # mise à jour de l'histogramme
+    updated_fig = px.histogram(
+        data_frame=filtered_df,
+        x='PTOT',
+        labels={'PTOT': 'Nombre d\'habitants', 'count': 'Nombre de défibrillateurs'},
+        color='REGION',
+        nbins=60
+    )
+    
+    return updated_fig
 
 if __name__ == '__main__':
     app.run(debug=True)
