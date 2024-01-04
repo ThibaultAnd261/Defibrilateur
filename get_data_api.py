aa0946ede504e58f87749516c2823e5327fff10f
ThibaultAnd261 <thibaultandwork@gmail.com>
2024-01-04 19:49:48 +0100

    Push for the histogram

--- get_data_api.py
+++ get_data_api.py
@@ -2,8 +2,10 @@ import csv
 import pandas as pd
 
 FILENAME_DEFIBRILLATEUR = "geodae.csv"
-FILENAME_COMMUNE = "communes.csv"
+FILENAME_COMMUNE_2019 = "communes.csv"
+FILENAME_COMMUNE_2021 = "donnees_communes.csv"
 
+# retourne le contenu du fichier sous forme de liste
 def read_file (filename):
     with open (filename, mode = 'r', encoding= 'utf8') as f :
         reader = csv.DictReader(f , delimiter = ";")
@@ -13,34 +15,26 @@ def read_file (filename):
     
     return l
 
-
-def population_category(ville):
-    if (int)(ville['PTOT']) < 5000:
-        return 'Village (entre 0 et 5000)'
-    elif (int)(ville['PTOT']) < 20000:
-        return 'Ville (entre 5000 et 20000)'
-    elif (int)(ville['PTOT']) < 50000:
-        return 'Ville moyenne (entre 20000 et 50000)'
-    elif (int)(ville['PTOT']) < 200000:
-        return 'Grande ville (entre 50000 et 200000)'
-    elif (int)(ville['PTOT']) >= 200000:
-        return 'Metropole (au-dessus de 200000)'
-
-
+# fusionne les dataframes en une seule manipulable
 def get_merged_dataframe():
     dataDefibrilateur = read_file(FILENAME_DEFIBRILLATEUR)
-    dfDef = pd.DataFrame(dataDefibrilateur)
+    dfDef = pd.DataFrame(dataDefibrilateur) # transformation de la data sur les défibrillateurs en dataframe 
 
-    dataCommune = read_file(FILENAME_COMMUNE)
-    dfCom = pd.DataFrame(dataCommune)
-
-    dfCom = dfCom.rename(columns={'DEPCOM': 'c_com_insee'})
-    merged_df = pd.merge(dfDef, dfCom, on='c_com_insee', how='inner')
-
-    # conversion en nombre
-    merged_df['PTOT'] = pd.to_numeric(merged_df['PTOT'])
-    merged_df['categoryVille'] = merged_df.apply(population_category, axis=1)
+    dataCommune2019 = read_file(FILENAME_COMMUNE_2019)
+    dfCom1 = pd.DataFrame(dataCommune2019)
+    dfCom1 = dfCom1.rename(columns={'DEPCOM': 'c_com_insee'}) # renommage de la colonne 'DEPCOM' par 'c_com_insee' pour faciliter le merge
     
+    dataCommune2021 = read_file(FILENAME_COMMUNE_2021)
+    dfCom2 = pd.DataFrame(dataCommune2021)
+    dfCom2['c_com_insee'] = dfCom2['CODDEP'] + dfCom2['CODCOM'].astype(str) # obtenir une colonne 'c_com_insee' sur dfCom2 pour faciliter le merge
+
+    merged_df = pd.merge(dfDef, dfCom1, on='c_com_insee', how='inner') # merge de df1 et df2 sur merged_df
+    merged_df = pd.merge(merged_df, dfCom2[['c_com_insee', 'REG']], on='c_com_insee', how='left') # merge de merged_df et df3 sur merged_df
+
+    merged_df['PTOT'] = pd.to_numeric(merged_df['PTOT']) # conversion en nombre
+    merged_df = merged_df.drop(['REG_y'], axis=1)
+    merged_df = merged_df.rename(columns={'REG_x' : 'REGION'})
+
     return merged_df
 
 
@@ -48,13 +42,20 @@ def get_merged_dataframe():
 #     dataDefibrilateur = read_file(FILENAME_DEFIBRILLATEUR)
 #     dfDef = pd.DataFrame(dataDefibrilateur)
 
-#     dataCommune = read_file(FILENAME_COMMUNE)
+#     dataCommune = read_file(FILENAME_COMMUNE_2019)
 #     dfCom = pd.DataFrame(dataCommune)
+    
+#     dataCommune2021 = read_file(FILENAME_COMMUNE_2021)
+#     dfCom2 = pd.DataFrame(dataCommune2021)
+#     dfCom2['c_com_insee'] = dfCom2['CODDEP'] + dfCom2['CODCOM'].astype(str)
 
 #     dfCom = dfCom.rename(columns={'DEPCOM': 'c_com_insee'})
 #     merged_df = pd.merge(dfDef, dfCom, on='c_com_insee', how='inner')
 
-#     merged_df['categoryVille'] = merged_df.apply(population_category, axis=1)
+#     merged_df = pd.merge(merged_df, dfCom2[['c_com_insee', 'REG']], on='c_com_insee', how='left')
+
+#     # merged_df['categoryVille'] = merged_df.apply(population_category, axis=1)
 #     print(merged_df)
+#     # print(dfCom2)
 
 # main()
--- main.py
+++ main.py
@@ -1,45 +1,51 @@
 from dash import Dash, html, dcc, callback, Output, Input, dash_table
 from get_data_api import get_merged_dataframe
 import plotly.express as px
+import numpy as np
+import folium
 
+# Dataframe mergée 
 df = get_merged_dataframe()
 
-addition = df.groupby('categoryVille').size()
-# villeName = df.groupby('categoryVille')['c_com_nom'].unique()
-villeName = df.groupby('categoryVille')['c_com_nom'].nunique()
+# création de l'histogramme par défaut
+fig = px.histogram(
+    data_frame=df,
+    x=df['PTOT'],
+    labels={'PTOT': 'Nombre d\'habitants', 'count': 'Nombre de défibrillateurs'},
+    color='REGION',
+    nbins=60
+)
 
-moyenne = addition / villeName
-
-fig = px.bar(x=moyenne.index, y=moyenne.values, labels={'x': 'Nombre d\'habitants','y': 'Nombre moyen de défibrillateur'})
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.H1(children='Title of Dash App', style={'textAlign':'center'}),
+    html.H1(children='Défibrillateur en France métropolitaine', style={'textAlign':'center'}),
     dcc.RadioItems(['Aucun filtre', 'Défibrillateurs validés', 'Défibrillateurs en attente'], 'Aucun filtre', inline=True, id='radio-value'),
     # dcc.Graph(figure=fig),
     dcc.Graph(figure=fig, id='graph-id'),
     dash_table.DataTable(data=df.to_dict('records'))
 ])
 
-@callback(
-    Output(component_id='graph-id', component_property='figure'),
-    Input(component_id='radio-value', component_property='value')
-)
-def update_graph(filter_selected):
-    if filter_selected == "Aucun filtre":
-        filtered_df = df
-    elif filter_selected == "Défibrillateurs validés":
-        filtered_df = df[df['c_etat_valid'] == 'validées']
-    else:
-        filtered_df = df[df['c_etat_valid'] == 'en attente de validation']
+# @callback(
+#     Output(component_id='graph-id', component_property='figure'),
+#     Input(component_id='radio-value', component_property='value')
+# )
+# def update_graph(filter_selected):
+#     if filter_selected == "Aucun filtre":
+#         filtered_df = df
+#     elif filter_selected == "Défibrillateurs validés":
+#         filtered_df = df[df['c_etat_valid'] == 'validées']
+#     else:
+#         filtered_df = df[df['c_etat_valid'] == 'en attente de validation']
     
-    addition_filtered = filtered_df.groupby('categoryVille').size()
-    villeName_filtered = filtered_df.groupby('categoryVille')['c_com_nom'].nunique()
-    moyenne_filtered = addition_filtered / villeName_filtered
+#     addition_filtered = filtered_df.groupby('categoryVille').size()
+#     villeName_filtered = filtered_df.groupby('categoryVille')['c_com_nom'].nunique()
+#     moyenne_filtered = addition_filtered / villeName_filtered
     
-    new_fig = px.bar(x=moyenne_filtered.index, y=moyenne_filtered.values, labels={'x': 'Nombre d\'habitants','y': 'Nombre moyen de défibrillateur'})
-    return new_fig
+#     # new_fig = px.bar(x=moyenne_filtered.index, y=moyenne_filtered.values, labels={'x': 'Nombre d\'habitants','y': 'Nombre moyen de défibrillateur'})
+#     new_fig = px.histogram(x=moyenne_filtered.index, y=moyenne_filtered.values, labels={'x': 'Nombre d\'habitants','y': 'Nombre moyen de défibrillateur'})
+#     return new_fig
 
 
 if __name__ == '__main__':
